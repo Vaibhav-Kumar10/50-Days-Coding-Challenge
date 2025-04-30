e0fe5cf96f8aee92f6a5974cca82a3210fc0b133
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-04-30 19:39:43 +0530

    Update Q2.cpp

---
+++ Day 1 - 30-04-2025/Q2/Q2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int total = 0, req = 0;
+        for(int i = 1; i <= nums.size(); i++){
+            total += i;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            req += nums[i];
+        }
+        return total - req;
+    }
+};
